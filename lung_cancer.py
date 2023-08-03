01378f18f128ff94d2e171b2e9c6de9d00a8fb90
manas95826 <74074241+manas95826@users.noreply.github.com>
2023-08-04 05:02:07 +0530

    Update lung_cancer.py

--- lung_cancer.py
+++ lung_cancer.py
@@ -31,7 +31,6 @@ def predict_lung_cancer_status(input_data):
     return int(prediction[0])
 
 def main():
-    st.set_theme("dark")
     st.title("Lung Cancer Prediction")
 
     st.sidebar.header("User Input")
