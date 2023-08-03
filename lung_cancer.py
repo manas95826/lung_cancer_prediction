ce6f700d5fc46ad3ef2504ca42a404d9d372d006
manas95826 <74074241+manas95826@users.noreply.github.com>
2023-08-04 05:24:21 +0530

    Update lung_cancer.py

--- lung_cancer.py
+++ lung_cancer.py
@@ -13,7 +13,7 @@ warnings.filterwarnings('ignore')
 # Load the trained model
 with open("lung_cancer.pkl", "rb") as f:
     model = pickle.load(f)
-
+# st.set_theme("dark")
 # Define default values for mean and variance
 default_mean = 0.0
 default_var = 1.0
@@ -36,19 +36,19 @@ def main():
     st.sidebar.header("User Input")
     age = st.sidebar.number_input("Age", min_value=0, max_value=100, step=1, key="age")
     gender = st.sidebar.selectbox("Gender", ["M", "F"], key="gender")
-    smoking = st.sidebar.number_input("Smoking", min_value=0, max_value=2, step=1, key="smoking")
-    yellow_fingers = st.sidebar.number_input("yellow_fingers", min_value=0, max_value=2, step=1, key="yellow_fingers")
-    anxiety = st.sidebar.number_input("anxiety",  min_value=0, max_value=2, step=1,key="anxiety")
-    chest_pain = st.sidebar.number_input("chest_pain", min_value=0, max_value=2, step=1, key="chest_pain")
-    swallowing_difficulty = st.sidebar.number_input("swallowing_difficulty", min_value=0, max_value=2, step=1, key="swallowing_difficulty")
-    shortness_of_breath = st.sidebar.number_input("shortness_of_breath", min_value=0, max_value=2, step=1, key="shortness_of_breath")
-    peer_pressure = st.sidebar.number_input("peer_pressure",  min_value=0, max_value=2, step=1,key="peer_pressure")
-    chronic_disease = st.sidebar.number_input("chronic_disease", min_value=0, max_value=2, step=1, key="chronic_disease")
-    coughing = st.sidebar.number_input("coughing",  min_value=0, max_value=2, step=1,key="coughing")
-    alcohol_consumption = st.sidebar.number_input("alcohol_consumption", min_value=0, max_value=2, step=1, key="alcohol_consumption")
-    wheezing = st.sidebar.number_input("wheezing", min_value=0, max_value=2, step=1, key="wheezing")
-    allergy = st.sidebar.number_input("allergy",  min_value=0, max_value=2, step=1,key="allergy")
-    fatigue = st.sidebar.number_input("fatigue", min_value=0, max_value=2, step=1, key="fatigue")
+    smoking = st.sidebar.selectbox("Smoking Frequency in a Day", ["0","1", "2"], key="smoking")
+    yellow_fingers = st.sidebar.selectbox("How many Yellow Fingers", ["0","1", "2"], key="yellow_fingers")
+    anxiety = st.sidebar.selectbox("Feeling of Anxiety in a Day", ["0","1", "2"],key="anxiety")
+    chest_pain = st.sidebar.selectbox("Chest Pain Frequency in a Day", ["0","1", "2"], key="chest_pain")
+    swallowing_difficulty = st.sidebar.selectbox("Swallowing Difficulty in a Day", ["0","1", "2"], key="swallowing_difficulty")
+    shortness_of_breath = st.sidebar.selectbox("Shortness of Breath in a Day", ["0","1", "2"], key="shortness_of_breath")
+    peer_pressure = st.sidebar.selectbox("Feeling of Peer Pressure Frequency in a Day", ["0","1", "2"],key="peer_pressure")
+    chronic_disease = st.sidebar.selectbox("How many Chronic Diseases", ["0","1", "2"], key="chronic_disease")
+    coughing = st.sidebar.selectbox("Coughing in a Day", ["0","1", "2"],key="coughing")
+    alcohol_consumption = st.sidebar.selectbox("Alcohol Consumption in a Day", ["0","1", "2"], key="alcohol_consumption")
+    wheezing = st.sidebar.selectbox("Wheezing Frequency in a Day", ["0","1", "2"], key="wheezing")
+    allergy = st.sidebar.selectbox("Number of Allergies", ["0","1", "2"],key="allergy")
+    fatigue = st.sidebar.selectbox("Feeling of Fatigue in a Day", ["0","1", "2"], key="fatigue")
 
     input_data = {
     'GENDER': gender,
