064204e7da9b7993967b18123d38ca469dedbe80
manas95826 <74074241+manas95826@users.noreply.github.com>
2023-08-04 23:02:48 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,32 +1,14 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
-import os
 import pickle
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-import warnings
-warnings.filterwarnings('ignore')
+import pandas as pd
 
 # Load the trained model
 with open("lung_cancer.pkl", "rb") as f:
     model = pickle.load(f)
-# st.set_theme("dark")
-# Define default values for mean and variance
-default_mean = 0.0
-default_var = 1.0
-
-# Create a StandardScaler instance with default values
-# scaler = StandardScaler()
-# scaler.mean_ = default_mean
-# scaler.var_ = default_var
 
 def predict_lung_cancer_status(input_data):
     input_df = pd.DataFrame([input_data])
-    input_df.GENDER.replace({'M': '1', 'F': '0'}, inplace=True)
-    # input_scaled = scaler.transform(input_df)
+    input_df['GENDER'].replace({'M': 1, 'F': 0}, inplace=True)
     prediction = model.predict(input_df)
     return int(prediction[0])
 
@@ -35,43 +17,46 @@ def main():
 
     st.sidebar.header("User Input")
     age = st.sidebar.number_input("Age", min_value=0, max_value=100, step=1, key="age")
-    gender = st.sidebar.selectbox("Gender", ["M", "F"], key="gender")
-    smoking = st.sidebar.selectbox("Smoking Frequency in a Day", ["0","1", "2"], key="smoking")
-    yellow_fingers = st.sidebar.selectbox("How many Yellow Fingers", ["0","1", "2"], key="yellow_fingers")
-    anxiety = st.sidebar.selectbox("Feeling of Anxiety in a Day", ["0","1", "2"],key="anxiety")
-    chest_pain = st.sidebar.selectbox("Chest Pain Frequency in a Day", ["0","1", "2"], key="chest_pain")
-    swallowing_difficulty = st.sidebar.selectbox("Swallowing Difficulty in a Day", ["0","1", "2"], key="swallowing_difficulty")
-    shortness_of_breath = st.sidebar.selectbox("Shortness of Breath in a Day", ["0","1", "2"], key="shortness_of_breath")
-    peer_pressure = st.sidebar.selectbox("Feeling of Peer Pressure Frequency in a Day", ["0","1", "2"],key="peer_pressure")
-    chronic_disease = st.sidebar.selectbox("How many Chronic Diseases", ["0","1", "2"], key="chronic_disease")
-    coughing = st.sidebar.selectbox("Coughing in a Day", ["0","1", "2"],key="coughing")
-    alcohol_consumption = st.sidebar.selectbox("Alcohol Consumption in a Day", ["0","1", "2"], key="alcohol_consumption")
-    wheezing = st.sidebar.selectbox("Wheezing Frequency in a Day", ["0","1", "2"], key="wheezing")
-    allergy = st.sidebar.selectbox("Number of Allergies", ["0","1", "2"],key="allergy")
-    fatigue = st.sidebar.selectbox("Feeling of Fatigue in a Day", ["0","1", "2"], key="fatigue")
+    gender = st.sidebar.radio("Gender", ["M", "F"], key="gender")
+    smoking = st.sidebar.selectbox("Smoking Frequency in a Day", ["0", "1", "2"], key="smoking")
+    yellow_fingers = st.sidebar.selectbox("How many Yellow Fingers", ["0", "1", "2"], key="yellow_fingers")
+    anxiety = st.sidebar.selectbox("Feeling of Anxiety in a Day", ["0", "1", "2"], key="anxiety")
+    chest_pain = st.sidebar.selectbox("Chest Pain Frequency in a Day", ["0", "1", "2"], key="chest_pain")
+    swallowing_difficulty = st.sidebar.selectbox("Swallowing Difficulty in a Day", ["0", "1", "2"], key="swallowing_difficulty")
+    shortness_of_breath = st.sidebar.selectbox("Shortness of Breath in a Day", ["0", "1", "2"], key="shortness_of_breath")
+    peer_pressure = st.sidebar.selectbox("Feeling of Peer Pressure Frequency in a Day", ["0", "1", "2"], key="peer_pressure")
+    chronic_disease = st.sidebar.selectbox("How many Chronic Diseases", ["0", "1", "2"], key="chronic_disease")
+    coughing = st.sidebar.selectbox("Coughing in a Day", ["0", "1", "2"], key="coughing")
+    alcohol_consumption = st.sidebar.selectbox("Alcohol Consumption in a Day", ["0", "1", "2"], key="alcohol_consumption")
+    wheezing = st.sidebar.selectbox("Wheezing Frequency in a Day", ["0", "1", "2"], key="wheezing")
+    allergy = st.sidebar.selectbox("Number of Allergies", ["0", "1", "2"], key="allergy")
+    fatigue = st.sidebar.selectbox("Feeling of Fatigue in a Day", ["0", "1", "2"], key="fatigue")
 
     input_data = {
-    'GENDER': gender,
-    'AGE': age,
-    'SMOKING': smoking,
-    'YELLOW_FINGERS': yellow_fingers,
-    'ANXIETY': anxiety,
-    'PEER_PRESSURE': peer_pressure,
-    'CHRONIC DISEASE': chronic_disease,
-    'WHEEZING': wheezing,
-    'ALCOHOL CONSUMING': alcohol_consumption,
-    'COUGHING': coughing,
-    'SHORTNESS OF BREATH': shortness_of_breath,
-    'SWALLOWING DIFFICULTY': swallowing_difficulty,
-    'CHEST PAIN': chest_pain
-}
-    probability=model.predict_proba([input_data])
+        'GENDER': gender,
+        'AGE': age,
+        'SMOKING': smoking,
+        'YELLOW_FINGERS': yellow_fingers,
+        'ANXIETY': anxiety,
+        'CHEST_PAIN': chest_pain,
+        'SWALLOWING_DIFFICULTY': swallowing_difficulty,
+        'SHORTNESS_OF_BREATH': shortness_of_breath,
+        'PEER_PRESSURE': peer_pressure,
+        'CHRONIC_DISEASE': chronic_disease,
+        'COUGHING': coughing,
+        'ALCOHOL_CONSUMPTION': alcohol_consumption,
+        'WHEEZING': wheezing,
+        'ALLERGY': allergy,
+        'FATIGUE': fatigue,
+    }
+
+    probability = model.predict_proba([input_data])
     result = predict_lung_cancer_status(input_data)
 
     if result == 1:
-        st.write("Prediction: You've Lung Cancer! Bye bye tata, good bye gaya! with probability",probability)
+        st.write("Prediction: You have Lung Cancer! Please consult a doctor. Probability:", probability[0][1])
     else:
-        st.write("Prediction: You don't have lung cancer chill, have good sex! with probability",probability)
+        st.write("Prediction: You don't have lung cancer. Probability:", probability[0][0])
 
 if __name__ == "__main__":
     main()
